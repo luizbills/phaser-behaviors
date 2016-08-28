dd837dacdaa202fa2cde36b78740d0f1243f2403
Luiz Paulo "Bills <luizpbills@gmail.com>
2016-08-28 18:15:28 -0300

    Create bullet-no-physics.js

---
+++ bullet/bullet-no-physics.js
@@ -0,0 +1,26 @@
+var Bullet = {
+
+  // defaults settings
+  options: {
+    angle: 0,
+    speed: 100,
+    rotate: true
+  },
+
+  create: function(entity, opts) {
+    if (opts.rotate) {
+      entity.rotation = opts.angle;
+    }
+  },
+
+  update: function(entity, opts) {
+    var game = entity.game
+    entity.x += Math.cos(opts.angle) * opts.speed * game.time.physicsElapsed
+    entity.y += Math.sin(opts.angle) * opts.speed * game.time.physicsElapsed
+  }
+}
+
+if (window.Phaser) {
+  Phaser.Behavior = Phaser.Behavior || {}
+  Phaser.Behavior.Bullet = Bullet
+}
