759b5f4b421ed650e4f799da688b3ea8656ba53f
Luiz Paulo "Bills <luizpbills@gmail.com>
2016-12-29 11:24:00 -0200

    Update bullet-no-physics.js

--- bullet/bullet-no-physics.js
+++ bullet/bullet-no-physics.js
@@ -1,26 +1,42 @@
-var Bullet = {
+;(function (root, factory, NULL) {
+    var BEHAVIOR_GLOBAL_NAME = 'Bullet';
 
-  // defaults settings
-  options: {
-    angle: 0,
-    speed: 100,
-    rotate: true
-  },
-
-  create: function(entity, opts) {
-    if (opts.rotate) {
-      entity.rotation = opts.angle
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        var Phaser = root.Phaser;
+        if (Phaser === NULL) {
+            root[BEHAVIOR_GLOBAL_NAME] = factory();
+        } else {
+            Phaser.Behavior = Phaser.Behavior || {};
+            Phaser.Behavior[BEHAVIOR_GLOBAL_NAME] = factory();
+        }
     }
-  },
+}(this, function (NULL) {
 
-  update: function(entity, opts) {
-    var game = entity.game
-    entity.x += Math.cos(opts.angle) * opts.speed * game.time.physicsElapsed
-    entity.y += Math.sin(opts.angle) * opts.speed * game.time.physicsElapsed
-  }
-}
+    var Behavior = {
 
-if (window.Phaser) {
-  Phaser.Behavior = Phaser.Behavior || {}
-  Phaser.Behavior.Bullet = Bullet
-}
+        // defaults settings
+        options: {
+            angle: 0,
+            speed: 100,
+            rotate: true
+        },
+
+        create: function(object, opts) {
+            if (opts.rotate) {
+                object.rotation = opts.angle
+            }
+        },
+
+        preUpdate: function(object, opts) {
+            var game = object.game
+            object.x += Math.cos(opts.angle) * opts.speed * game.time.physicsElapsed
+            object.y += Math.sin(opts.angle) * opts.speed * game.time.physicsElapsed
+        }
+    };
+
+    return Behavior;
+}));
