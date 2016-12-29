6bb36c7eedadf0737f89b2d3a7a9ae1e42c0f39d
Luiz Paulo "Bills <luizpbills@gmail.com>
2016-12-29 11:06:16 -0200

    Update behavior-template.js

--- behavior-template.js
+++ behavior-template.js
@@ -1,4 +1,4 @@
-;(function (root, factory) {
+;(function (root, factory, NULL) {
     var BEHAVIOR_GLOBAL_NAME = 'ChangeThis';
     
     if (typeof define === 'function' && define.amd) {
@@ -14,42 +14,43 @@
             Phaser.Behavior[BEHAVIOR_GLOBAL_NAME] = factory();
         }
   }
-}(this, function () {
+}(this, function (NULL) {
 
     var Behavior = {
 
-      // default settings
-      options: {
-        opt1: 'value',
-        opt2: 0,
-        opt3: [true, 23, 'foo']
-      },
+        // default settings
+        options: {
+            opt1: 'value',
+            opt2: 0,
+            opt3: [true, 23, 'foo']
+        },
 
-      create: function(object, opts) {
-        // called when the behavior is ADDED to a game object
-      },
+        create: function(object, opts) {
+            // called when the behavior is ADDED to a game object
+        },
 
-      destroy: function(object, opts) {
-        // called when the behavior is REMOVED from a game object
-      },
+        destroy: function(object, opts) {
+            // called when the behavior is REMOVED from a game object
+        },
 
-      preUpdate: function(object, opts) {
-        // called at the very start of the update cycle,
-        // before any other subsystems have been updated (including Physics)
-      },
+        preUpdate: function(object, opts) {
+            // called at the very start of the update cycle,
+            // before any other subsystems have been updated (including Physics)
+        },
 
-      update: function(object, opts) {
-        // called after all the core subsystems (Input, Tweens, Sound, etc)
-        // and the State have updated, but before the render
-      },
+        update: function(object, opts) {
+            // called after all the core subsystems (Input, Tweens, Sound, etc)
+            // and the State have updated, but before the render
+        },
 
-      render: function(object, opts) {
-        // called right after the Game Renderer completes, but before the State.render
-      },
+        render: function(object, opts) {
+            // called right after the Game Renderer completes, but before the State.render
+        },
 
-      postRender: function(object, opts) {
-        // called after the Game Renderer and State.render have run
-      }
+        postRender: function(object, opts) {
+            // called after the Game Renderer and State.render have run
+        }
+
     };
     
     return Behavior;
