e38c2b98c2e6424559a0020d4332b1fd5ec0c561
Oli4242 <Oli4242@users.noreply.github.com>
2017-06-15 22:20:45 +0200

    using the template and stop using `this`

--- platformer/platformer.js
+++ platformer/platformer.js
@@ -1,59 +1,32 @@
-var Platformer = {
+;(function (root, factory, NULL) {
+    var BEHAVIOR_GLOBAL_NAME = 'Platformer';
 
-    // defaults settings
-    options: {
-        gravity: 600,
-        velocity: 200,
-        jumpStrength: 250,
-        controls: {
-            left: [Phaser.KeyCode.LEFT],
-            right: [Phaser.KeyCode.RIGHT],
-            jump: [Phaser.KeyCode.UP]
-        }
-    },
-
-    create: function (object, options) {
-        var gravity = options.gravity;
-        var controls = options.controls;
-
-        if (gravity > 0) {
-            object.body.gravity.y = gravity;
-        }
-
-        options._control_keys = {
-            left: this._addKeys(object.game, controls.left),
-            right: this._addKeys(object.game, controls.right),
-            jump: this._addKeys(object.game, controls.jump)
-        };
-    },
-
-    preUpdate: function (object, options) {
-        var controlKeys = options._control_keys;
-        var velocity = options.velocity;
-
-        if (this._isDown(controlKeys.left)) {
-            object.body.velocity.x = -velocity;
-        } else if (this._isDown(controlKeys.right)) {
-            object.body.velocity.x = velocity;
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        var Phaser = root.Phaser;
+        if (Phaser === NULL) {
+            root[BEHAVIOR_GLOBAL_NAME] = factory();
         } else {
-            object.body.velocity.x = 0;
+            Phaser.Behavior = Phaser.Behavior || {};
+            Phaser.Behavior[BEHAVIOR_GLOBAL_NAME] = factory();
         }
+    }
+}(this, function (NULL) {
+    'use strict';
 
-        if (this._isDown(controlKeys.jump) && (object.body.touching.down || object.body.blocked.down)) {
-            object.body.velocity.y = -options.jumpStrength;
-        }
-    },
-
-    _addKeys: function(game, keyCodes) {
+    function addKeys(game, keyCodes) {
         if (!Array.isArray(keyCodes)) {
             keyCodes = [keyCodes];
         }
         return keyCodes.map(function(keyCode) {
             return game.input.keyboard.addKey(keyCode);
         });
-    },
+    }
 
-    _isDown: function(keys) {
+    function isDown(keys) {
         for (let key of keys) {
             if (key.isDown) {
                 return true;
@@ -61,9 +34,53 @@ var Platformer = {
         }
         return false;
     }
-};
 
-if (window.Phaser) {
-  Phaser.Behavior = Phaser.Behavior || {};
-  Phaser.Behavior.Platformer = Platformer;
-}
+    var Behavior = {
+
+        // default settings
+        options: {
+            gravity: 600,
+            velocity: 200,
+            jumpStrength: 250,
+            controls: {
+                left: [Phaser.KeyCode.LEFT],
+                right: [Phaser.KeyCode.RIGHT],
+                jump: [Phaser.KeyCode.UP]
+            }
+        },
+
+        create: function(object, options) {
+            var gravity = options.gravity;
+            var controls = options.controls;
+
+            if (gravity > 0) {
+                object.body.gravity.y = gravity;
+            }
+
+            options._control_keys = {
+                left: addKeys(object.game, controls.left),
+                right: addKeys(object.game, controls.right),
+                jump: addKeys(object.game, controls.jump)
+            };
+        },
+
+        preUpdate: function(object, options) {
+            var controlKeys = options._control_keys;
+            var velocity = options.velocity;
+
+            if (isDown(controlKeys.left)) {
+                object.body.velocity.x = -velocity;
+            } else if (isDown(controlKeys.right)) {
+                object.body.velocity.x = velocity;
+            } else {
+                object.body.velocity.x = 0;
+            }
+
+            if (isDown(controlKeys.jump) && (object.body.touching.down || object.body.blocked.down)) {
+                object.body.velocity.y = -options.jumpStrength;
+            }
+        }
+    };
+
+    return Behavior;
+}));
